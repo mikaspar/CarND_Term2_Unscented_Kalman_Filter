e61c7e46adaf34b6d9d35897cf93c731120b8a63
mikaspar <36734496+mikaspar@users.noreply.github.com>
2018-08-13 16:46:17 +0200

    Add files via upload

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,33 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+
+	rmse << 0, 0, 0, 0;
+
+    if(estimations.empty()) {
+	  cout << "No estimations delivered" << endl;
+      return rmse;
+    }
+    if (estimations.size() != ground_truth.size())	{
+	  cout << "Number of estimations does not fit the number of ground truth values" << endl;
+	  return rmse;
+	}
+
+	//sum of the differences
+	for(unsigned int i=0; i < estimations.size(); ++i){
+
+		VectorXd difference = estimations[i] - ground_truth[i];
+		difference = difference.array()*difference.array();
+		rmse += difference;
+	}
+
+	//calculate the mean
+	rmse = rmse/estimations.size();
+
+	//calculate the squared root
+	rmse = rmse.array().sqrt();
+
+	//return the result
+	return rmse;
 }
--- src/ukf.h
+++ src/ukf.h
@@ -14,13 +14,13 @@ class UKF {
 public:
 
   ///* initially set to false, set to true in first call of ProcessMeasurement
-  bool is_initialized_;
+  bool is_initialized;
 
   ///* if this is false, laser measurements will be ignored (except for init)
-  bool use_laser_;
+  bool use_laser;
 
   ///* if this is false, radar measurements will be ignored (except for init)
-  bool use_radar_;
+  bool use_radar;
 
   ///* state vector: [pos1 pos2 vel_abs yaw_angle yaw_rate] in SI units and rad
   VectorXd x_;
@@ -32,40 +32,61 @@ public:
   MatrixXd Xsig_pred_;
 
   ///* time when the state is true, in us
-  long long time_us_;
+  long long previous_timestamp;
 
   ///* Process noise standard deviation longitudinal acceleration in m/s^2
-  double std_a_;
+  double std_a;
 
   ///* Process noise standard deviation yaw acceleration in rad/s^2
-  double std_yawdd_;
+  double std_yawdd;
 
   ///* Laser measurement noise standard deviation position1 in m
-  double std_laspx_;
+  double std_laspx;
 
   ///* Laser measurement noise standard deviation position2 in m
-  double std_laspy_;
+  double std_laspy;
 
   ///* Radar measurement noise standard deviation radius in m
-  double std_radr_;
+  double std_radr;
 
   ///* Radar measurement noise standard deviation angle in rad
-  double std_radphi_;
+  double std_radphi;
 
   ///* Radar measurement noise standard deviation radius change in m/s
-  double std_radrd_ ;
+  double std_radrd ;
 
   ///* Weights of sigma points
   VectorXd weights_;
 
+   ///* Radar measurement noise
+  MatrixXd R_radar_;
+
+   ///* Lidar measurement noise
+  MatrixXd R_laser_;
+
+  ///* H Laser
+  
+  MatrixXd H_laser_;
+  
+  
   ///* State dimension
-  int n_x_;
+  int n_x;
 
   ///* Augmented state dimension
-  int n_aug_;
+  int n_aug;
+
+  ///* Augmented sigma points dimension
+  int n_sig;
+
 
   ///* Sigma point spreading parameter
-  double lambda_;
+  double lambda;
+
+
+ ///* NIS
+
+  double NIS_laser;
+  double NIS_radar;
 
 
   /**
